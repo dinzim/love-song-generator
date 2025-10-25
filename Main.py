75c3f5d8f1c27765c9b4a788e79143acc6451dcd
Dina Zimmermann <dinalongva@gmail.com>
2025-10-25 14:57:06 +0200

    Created UI

--- Main.py
+++ Main.py
@@ -2,41 +2,22 @@ import random as rand
 from WordState import WordState
 import pandas as pd
 from collections import defaultdict
-    
-# Some sentences to test your code on...
-test_sentences = ["hello there friend",
-                  "hello there good friend",
-                  "hello there my good friend",
-                  "hello my friend",
-                  "hello my good friend",
-                  "good day friend",
-                  "good morning friend",
-                  "good morning to you my friend",
-                  "good morning to you my good friend"]
-    
-# A simple "pre-trained model"/graph derived from above sentences with words and corresponding frequencies.
-states = {"#": {"hello": 5, "good": 4},
-        "hello": {"there": 3, "my": 2},
-        "good": {"friend": 4, "day": 1, "morning": 3},
-        "there": {"friend": 1, "good": 1, "my": 1},
-        "my": {"good": 2, "friend": 2},
-        "friend": {},
-        "day": {"friend": 1},
-        "morning": {"friend": 1, "to": 2},
-        "to": {"you": 2},
-        "you": {"my": 1}}
  
 #Train Markov chain from csv file
 def markov_chain_from_csv(filepath):
       data = pd.read_csv(filepath, usecols=[0], names=["Title"], header=None) #read csv
-   
+
+      #clean data
+      data['Title'] = data['Title'].str.strip() #remove spaces in beginning or end
+      data = data[data['Title'].str.contains(' ')]  #removes one-word titles, by keeping with at least one space
+
       chain = defaultdict(WordState) #dict to store words and frequencies
       
       #create Markov chain
       for sentence in data["Title"]: 
-        words = sentence.strip().split()        #list of words
-        words = ["#"] + words           #add "#" as first word
-        for i in range(len(words) - 1):         #iterate over data
+        words = sentence.split() #list of words
+        words = ["#"] + words  #add "#" as first word
+        for i in range(len(words) - 1):    #iterate over data
                 current, nxt = words[i], words[i+1]
                 chain[current].add_next_word(nxt)
 
@@ -58,10 +39,48 @@ def generate_new_sentence(chain):
      return output
 
 def main():
+        print("--- Love Song Title Generator ---")
+
         path = "love_song_data.csv"
         chain = markov_chain_from_csv(path)
-        love_song = generate_new_sentence(chain)
-        print(love_song)
+        num_titles = 1
+        store_songs = []
+
+        print(f"Press Enter to generate {num_titles} love song title(s)")
+        print("Type 's' for settings")
+        print("Type 'q' to quit")
+        print("Type 'f' so save love song titles to a file")
+
+        while True:
+                cmd = input(":")
+                
+                if cmd == "q":
+                        print("Goodbye!")
+                        break
+                elif cmd == "s":
+                        print("---Settings---")
+                        print("1 | Training data path:", path)
+                        print("2 | Number of sentences:", num_titles)
+                        choice = input("Enter setting number to change: ")
+
+                        if choice == "1":
+                            path = str(input("Enter new path: "))
+                            chain = markov_chain_from_csv(path)
+                            print("Setting saved!")
+
+                        if choice == "2":
+                            num_titles = int(input("Enter number of sentences: "))
+                            print("Setting saved!")
+                elif cmd == "f":
+                        with open("generated_titles.txt", "w", encoding="utf-8") as f:
+                              for title in store_songs:
+                                    f.write(title + "\n")
+                        print("Titles successfully stored in file 'generated_titles.txt'")
+                else:
+                        for _ in range(num_titles):
+                            love_song = generate_new_sentence(chain)
+                            store_songs.append(love_song)
+                            print(love_song)
 
 if __name__ == "__main__":
      main()
