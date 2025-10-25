b86212c29ebb83c8385042f323ef5c106f43d719
Dina Zimmermann <dinalongva@gmail.com>
2025-10-25 13:50:52 +0200

    Updated Main.py and added .gitignore

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import random as rand
-from WordState import WordState # Use as needed.
+from WordState import WordState
+import pandas as pd
+from collections import defaultdict
     
 # Some sentences to test your code on...
 test_sentences = ["hello there friend",
@@ -23,6 +25,43 @@ states = {"#": {"hello": 5, "good": 4},
         "morning": {"friend": 1, "to": 2},
         "to": {"you": 2},
         "you": {"my": 1}}
-    
-# Write your implementation here...
-            
+ 
+#Train Markov chain from csv file
+def markov_chain_from_csv(filepath):
+      data = pd.read_csv(filepath, usecols=[0], names=["Title"], header=None) #read csv
+   
+      chain = defaultdict(WordState) #dict to store words and frequencies
+      
+      #create Markov chain
+      for sentence in data["Title"]: 
+        words = sentence.strip().split()        #list of words
+        words = ["#"] + words           #add "#" as first word
+        for i in range(len(words) - 1):         #iterate over data
+                current, nxt = words[i], words[i+1]
+                chain[current].add_next_word(nxt)
+
+      return chain
+
+#Generate new love song
+def generate_new_sentence(chain):
+     word = "#" #start of chain
+     output = ""
+     max_length = 10
+
+     for _ in range(max_length):
+        if word not in chain or not chain[word].has_next():
+               break
+        next_word = chain[word].get_next()
+        output += next_word + " "
+        word = next_word
+     
+     return output
+
+def main():
+        path = "love_song_data.csv"
+        chain = markov_chain_from_csv(path)
+        love_song = generate_new_sentence(chain)
+        print(love_song)
+
+if __name__ == "__main__":
+     main()
