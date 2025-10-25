aeb4652f6e37981519be45fa96c79de334277a9e
Dina Zimmermann <dinalongva@gmail.com>
2025-10-25 13:00:53 +0200

    Updated WordState.py

--- WordState.py
+++ WordState.py
@@ -10,14 +10,18 @@ class WordState:
     def add_next_word(self, next_word):
         """Introduces a new next word to the current word.
         If the word already exists in the dict, its count is incremented."""
-        pass
+        self._next_words[next_word] = self._next_words.get(next_word, 0) + 1
             
     def has_next(self):
         """True if there are any more words following this one."""
-        return False
+        return bool(self._next_words)
         
     def get_next(self):
         """Returns a random next word based on probability."""
-        return None
-
+        #weighted random sampling
+        next_word = rand.choices(
+            list(self._next_words.keys()), #population is the words
+            weights = list(self._next_words.values()), #weights are the number of times they are counted
+            k = 1)[0] #only get one word as return
+        return next_word
             
